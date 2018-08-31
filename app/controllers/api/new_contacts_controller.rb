e25fcd0fc96b054608f3c4779c1765b0f904fb99
Nakamal <nickhaugland@gmail.com>
2018-08-30 22:56:16 -0500

    update complete

--- app/controllers/api/new_contacts_controller.rb
+++ app/controllers/api/new_contacts_controller.rb
@@ -1,23 +1,33 @@
 class Api::NewContactsController < ApplicationController
   def index
-    @person = Person.first
-    render "one_contact.json.jbuilder"
+    @person = Person.find(params[:id])
+    render "show.json.jbuilder"
   end
 
   def create
-    @people = Person.all
-    render "many_contacts.json.jbuilder"
+    @person = Person.find(params[:id])
+    render "show.json.jbuilder"
   end
 
   def show
-
+    @person = Person.find(params[:id])
+    render "show.json.jbuilder"
   end
 
   def update
+    @person = Person.find(params[:id])
 
+    @person.first_name = params[:first_name] || @person.first_name
+    @person.last_name = params[:last_name] || @person.last_name
+    @person.email = params[:email] || @person.email
+    @person.phone_numnber = params[:phone_numnber] || @person.phone_numnber
+
+    @person.save
+    render "show.json.jbuilder"
   end
 
   def destroy
-    
+    @person = Person.find(params[:id])
+    render "show.json.jbuilder"
   end
 end
